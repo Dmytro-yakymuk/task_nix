1c80300e60a9d262a2cd1b05a7e674c0073c4ac1
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2021-01-29 14:58:04 +0200

    task2 done

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	url = "https://jsonplaceholder.typicode.com"
+)
 
 func main() {
-	fmt.Println("Hello, NIX Education")
+	resp, err := http.Get(url + "/posts")
+	if err != nil {
+		log.Fatalf("Error GET request in address %s: %s", url+"/posts", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err.Error())
+	}
+
+	fmt.Printf("%s\n", body)
 }
